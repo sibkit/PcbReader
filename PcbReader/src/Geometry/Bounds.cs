5b4a1a26edada837122f19b9993aad8a5fb3cfa3
sibkit <sibkit@hotmail.com>
2025-05-21 01:53:18 +0700

    Bounds optimized

--- PcbReader/src/Geometry/Bounds.cs
+++ PcbReader/src/Geometry/Bounds.cs
@@ -8,12 +8,10 @@ public readonly struct Bounds {
     public double MaxY { get; }
 
     public Point MinPoint => new(MinX, MinY);
-
     public Point MaxPoint => new(MaxX, MaxY);
-
-
-    public Bounds(Point minPoint, Point maxPoint) :this(minPoint.X, minPoint.Y, maxPoint.X, maxPoint.Y) { }
     
+    public Bounds(Point minPoint, Point maxPoint) : this(minPoint.X, minPoint.Y, maxPoint.X, maxPoint.Y) { }
+
     public Bounds(double minX, double minY, double maxX, double maxY) {
         if(minX > maxX || minY > maxY)
             throw new Exception("Bounds: minPoint must be greater than or equal to maxPoint");
@@ -24,22 +22,18 @@ public readonly struct Bounds {
     }
     
     public double GetWidth() {
-        return MaxPoint.X - MinPoint.X;
+        return MaxX - MinX;
     }
 
     public double GetHeight() {
-        return MaxPoint.Y - MinPoint.Y;
+        return MaxY - MinY;
     }
 
     public bool Contains(Point p) {
-        return p.X > MinPoint.X && p.X < MaxPoint.X && p.Y > MinPoint.Y && p.Y < MaxPoint.Y;
+        return p.X > MinX && p.X < MaxX && p.Y > MinY && p.Y < MaxY;
     }
-
-    private static bool IsIntersectedByAxe(double aMin, double aMax, double bMin, double bMax) {
-        return !(aMax < bMin) && !(bMax < aMin);
-    }
+    
     public bool IsIntersected(Bounds b) {
-        return IsIntersectedByAxe(MinPoint.X, MaxPoint.X, b.MinPoint.X, b.MaxPoint.X) && 
-               IsIntersectedByAxe(MinPoint.Y, MaxPoint.Y, b.MinPoint.Y, b.MaxPoint.Y);
+        return !((MaxX<b.MinX || b.MaxX<MinX) && (MaxY<b.MaxY || b.MaxY<MinY));
     }
 }
--- PcbReaderTest/src/GeometryTest/BoundsTest.cs
+++ PcbReaderTest/src/GeometryTest/BoundsTest.cs
@@ -5,6 +5,12 @@ namespace PcbReaderTest.GeometryTest;
 public class BoundsTest {
     [Fact]
     public void TestBoundsIntersections() {
-        var v1 = new Bounds();
+        var b1 = new Bounds(0,0,5,5);
+        
+        Assert.False(b1.IsIntersected(new Bounds(6,6,7,7)));
+        Assert.True(b1.IsIntersected(new Bounds(-1,-1,7,7)));
+        Assert.True(b1.IsIntersected(new Bounds(-1,-1,1,1)));
     }
+    
+    
 }
